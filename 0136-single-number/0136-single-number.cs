7f8e7b60ced25a7003ad216c0c38f7d9a26b057d
Praneethlucky <38687138+Praneethlucky@users.noreply.github.com>
2024-06-23 18:02:00 +0530

    Time: 102 ms (58.68%), Space: 45.3 MB (88.45%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int ans = 0;
+        foreach(var x in nums)
+        {
+            ans = x^ans;
+        }
+        return ans;
+    }
+}
